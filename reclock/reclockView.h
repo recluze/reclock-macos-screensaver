a7fcde018664611cf653c662d5fdda45934d09fc
Nauman <recluze@gmail.com>
2021-12-30 10:47:26 +0500

    Initial Commit

---
+++ reclock/reclockView.h
@@ -0,0 +1,12 @@
+//
+//  reclockView.h
+//  reclock
+//
+//  Created by Mohammad Nauman on 30/12/2021.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface reclockView : ScreenSaverView
+
+@end
---
+++ reclock/reclockView.m
@@ -0,0 +1,51 @@
+//
+//  reclockView.m
+//  reclock
+//
+//  Created by Mohammad Nauman on 30/12/2021.
+//
+
+#import "reclockView.h"
+
+@implementation reclockView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
